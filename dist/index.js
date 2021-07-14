e9616e1080e062dd2c2cbcaedfd141386b238428
ilyaVynogradov <57397422+ilyaVynogradov@users.noreply.github.com>
2021-07-14 13:05:23 +0300

    upload

--- dist/index.js
+++ dist/index.js
@@ -14,18 +14,68 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const sharp_1 = __importDefault(require("sharp"));
+const file_type_1 = __importDefault(require("file-type"));
 const app = express_1.default();
 const port = 8000;
+var Resize;
+(function (Resize) {
+    Resize[Resize["finalWidth"] = 1400] = "finalWidth";
+    Resize[Resize["finalHeight"] = 700] = "finalHeight";
+})(Resize || (Resize = {}));
+;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 try {
     app.post('/upload/dog/image', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const result = yield (axios_1.default.get('https://random.dog/woof.json'));
         const fin = result.data;
-        const myUrl = fin.url;
-        console.log(myUrl);
+        const imgUrl = fin.url;
+        const downloadFile = (fileUrl, downloadFolder) => __awaiter(void 0, void 0, void 0, function* () {
+            const fileName = path_1.default.basename(fileUrl);
+            const localFilePath = path_1.default.resolve(__dirname, downloadFolder, fileName);
+            try {
+                const response = yield (axios_1.default.get(fileUrl, {
+                    responseType: 'stream',
+                }));
+                const w = response.data.pipe(fs_1.default.createWriteStream(localFilePath));
+                w.on('finish', () => {
+                    console.log('Image downloaded');
+                });
+            }
+            catch (error) {
+                console.log(`Error: ${error.message}`);
+            }
+        });
+        downloadFile(imgUrl, 'download');
+        let checkType = () => {
+            (() => __awaiter(void 0, void 0, void 0, function* () {
+                const type = yield file_type_1.default.fromFile(__dirname + '/download/' + path_1.default.basename(imgUrl));
+                console.log(type);
+                if (type.ext === 'jpg' || 'jpeg' || 'png') {
+                    console.log('Type is OK');
+                }
+                else {
+                    res.redirect('/upload/dog/image');
+                }
+            }))();
+        };
+        let resizePicture = () => {
+            let inputFile = __dirname + '/download/' + path_1.default.basename(imgUrl);
+            let outputFile = __dirname + '/download/resized/' + 'resized-' + path_1.default.basename(imgUrl);
+            sharp_1.default(inputFile).resize({ height: Resize.finalHeight, width: Resize.finalWidth }).toFile(outputFile)
+                .then(function (newFileInfo) {
+                console.log("Success");
+            })
+                .catch(function (error) {
+                console.log(`Error: ${error.message}`);
+            });
+        };
+        setTimeout(checkType, 1000);
+        setTimeout(resizePicture, 2000);
         return res.status(200).send({
-            message: "done",
             fin,
         });
     }));
--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
-import express, { Application, Request, response, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+import FileType from 'file-type';
+
 
 const app: Application = express();
 const port = 8000;
+
+enum Resize { finalWidth = 1400, finalHeight = 700 };
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,15 +22,60 @@ try{
 
       const fin = result.data;
 
-      const myUrl = fin.url;
+      const imgUrl = fin.url;
+      
+      const downloadFile = async(fileUrl: string, downloadFolder: string) => {
 
-      var img = new Image();
-      img.src = myUrl;
-      img.onload = () => {
-        var params = (this.width + ' ' + this.height)
-        
+        const fileName = path.basename(fileUrl);
+        const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
+
+        try {
+          const response = await (axios.get(
+            fileUrl, {
+              responseType: 'stream',
+            }
+            ));
+
+          const w = response.data.pipe(fs.createWriteStream(localFilePath));
+          w.on('finish', () => {
+            console.log('Image downloaded');
+          });
+
+        } catch (error) {
+          console.log(`Error: ${error.message}`);
+        }
+      }
+
+      downloadFile(imgUrl, 'download');
+
+      let checkType = () => {
+        (async () => {
+          const type = await FileType.fromFile(__dirname + '/download/'+ path.basename(imgUrl));
+          console.log(type);
+          if (type.ext == 'jpg' || 'jpeg' || 'png') {
+            console.log('Type is OK');
+          } else {
+            res.redirect('/upload/dog/image');
+          }
+        })();
       }
       
+      let resizePicture = () => {
+        let inputFile = __dirname + '/download/'+ path.basename(imgUrl);
+        let outputFile = __dirname+ '/download/resized/' +'resized-' + path.basename(imgUrl);
+
+        sharp(inputFile).resize({height: Resize.finalHeight, width: Resize.finalWidth }).toFile(outputFile)
+        .then(function(newFileInfo) {
+          console.log("Success");
+        })
+        .catch(function(error) {
+          console.log(`Error: ${error.message}`);
+        });
+      }
+
+      setTimeout(checkType, 1000);
+      setTimeout(resizePicture, 2000);
+
       return res.status(200).send({
         fin,
       });
